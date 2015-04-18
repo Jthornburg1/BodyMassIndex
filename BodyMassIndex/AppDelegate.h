b42872573a9b50dc16ef0f740ee846bdbb57942f
Jon Thornburg <reliable2192@gmail.com>
2015-04-18 17:57:52 -0400

    Initial Commit

---
+++ BodyMassIndex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BodyMassIndex
+//
+//  Created by jonathan thornburg on 4/18/15.
+//  Copyright (c) 2015 jonathan thornburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
